dfce39e09c1651c2ffed82401fa5c8353533d81f
web-dev-akash <akumar415129.ak@gmail.com>
2023-06-26 17:57:55 +0530

    Final Test Commit with Data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const axios = require("axios");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 require("dotenv").config();
 const app = express();
 app.use(express.json());
@@ -7,29 +9,26 @@ const PORT = 8080;
 const WATI_TOKEN = process.env.WATI_TOKEN;
 
 const sendMessageToUser = async (text) => {
-  try {
-    const url = `https://live-server-105694.wati.io/api/v1/sendSessionMessage/917018178377?messageText=Hello%20%5Cn%20How%20are%20you`;
-    const config = {
-      headers: {
-        Authorization: `Bearer ${WATI_TOKEN}`,
-      },
+  const messageText = `Hello, Thanks For Showing Interest in our Programme`;
+  if (text.toLowerCase() == "interested") {
+    const url = `https://live-server-105694.wati.io/api/v1/sendSessionMessage/917018178377?messageText=${messageText}`;
+    const options = {
+      method: "POST",
+      headers: { Authorization: `Bearer ${WATI_TOKEN}` },
     };
-    console.log("first");
-    const response = await axios.post(url, config);
-    console.log("second");
-    console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return error;
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+      .catch((err) => console.error("error:" + err));
   }
 };
 
 app.post("/", async (req, res) => {
   try {
     const { text } = req.body;
-    await sendMessageToUser(text);
+    const data = await sendMessageToUser(text);
     res.send({
+      data,
       status: "success",
     });
   } catch (error) {
